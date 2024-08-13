391843f01deaafed14cc5a41f69c674198e07f11
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-08-13 09:14:46 -0400

    Add active record callbacks

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,10 @@
 class Post < ActiveRecord::Base
 
   belongs_to :author
-  validate :is_title_case 
+  validate :is_title_case
+
+  before_validation :make_title_case
+  before_save :email_author_about_post
 
   private
 
@@ -9,6 +12,10 @@ class Post < ActiveRecord::Base
     if title.split.any?{|w|w[0].upcase != w[0]}
       errors.add(:title, "Title must be in title case")
     end
+  end
+
+  def email_author_about_post
+    # Not implemented.
   end
 
   def make_title_case
